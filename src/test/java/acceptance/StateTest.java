bcc575a9426a1dfd33bea36d934d2ca87685dfd0
Nkanyiso <nyawo023@student.wethinkcode.co.za>
2024-07-10 17:11:18 +0200

    adding state test for robot exists in the world

---
+++ src/test/java/acceptance/StateTest.java
@@ -0,0 +1,73 @@
+package acceptance;
+
+import client.RobotWorldClient;
+import client.RobotWorldJsonClient;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StateTest {
+    private final static int DEFAULT_PORT = 5000;
+    private final static String DEFAULT_IP = "localhost";
+    private final RobotWorldClient serverClient = new RobotWorldJsonClient();
+
+
+    @BeforeEach
+    void connectToServer() {
+        serverClient.connect(DEFAULT_IP, DEFAULT_PORT);
+    }
+
+    @AfterEach
+    void disconnectFromServer() {
+        serverClient.disconnect();
+    }
+
+
+    @Test
+    void robotExistsInWorld(){
+        assertTrue(serverClient.isConnected());
+
+        //    Given that I already exist in the World server
+        String launchRequest = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"launch\"," +
+                "  \"arguments\": [\"shooter\",\"2\",\"2\"]" +
+                "}";
+        JsonNode launchResponse = serverClient.sendRequest(launchRequest);
+
+        assertNotNull(launchResponse.get("result"));
+        assertEquals("OK", launchResponse.get("result").asText());
+
+        //    When I send a Valid state request to the Server
+        String stateRequest = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"state\"," +
+                "  \"arguments\": []" +
+                "}";
+        JsonNode stateResponse = serverClient.sendRequest(stateRequest);
+
+        //    Then I should get a valid response from the server
+        assertNotNull(stateResponse.get("result"));
+        assertEquals("OK", stateResponse.get("result").asText());
+        //    And I should get the state of the robot
+        assertNotNull(stateResponse.get("state"));
+        assertNotNull(stateResponse.get("state").get("position"));
+        assertEquals(0, stateResponse.get("state").get("position").get(0).asInt());
+        assertEquals(0, stateResponse.get("state").get("position").get(1).asInt());
+
+        assertNotNull(stateResponse.get("state").get("direction"));
+        assertEquals("NORTH", stateResponse.get("state").get("direction").asText());
+
+        assertNotNull(stateResponse.get("state").get("shields"));
+        assertEquals(0,stateResponse.get("state").get("shields").asInt());
+
+        assertNotNull(stateResponse.get("state").get("shots"));
+        assertEquals(0,stateResponse.get("state").get("shots").asInt());
+
+        assertNotNull(stateResponse.get("state").get("status"));
+        assertEquals("TODO", stateResponse.get("state").get("status").asText());
+    }
+}
