b0974970c249355fe93534c5722a498861b77ec0
Nkanyiso <nyawo023@student.wethinkcode.co.za>
2024-07-22 14:02:01 +0200

    Launch another robot in the world: still needs a little bit of tweaking

--- src/test/java/acceptance/world_2x2/LaunchRobotTests.java
+++ src/test/java/acceptance/world_2x2/LaunchRobotTests.java
@@ -32,6 +32,73 @@ class LaunchRobotTests {
     }
 
     @Test
+    void launchAnotherRobot() {
+        // Given a world of size 2x2
+        assertTrue(serverClient.isConnected());
+
+        // and robot "HAL" has already been launched into the world
+        String Request1 = "{" +
+                "\"robot\": \"HAL\"," +
+                "\"command\": \"launch\"," +
+                "\"arguments\": [\"shooter\",\"0\",\"0\"]" +
+                "}";
+
+        JsonNode Response = serverClient.sendRequest(Request1);
+        System.out.println(Response);
+        assertNotNull(Response.get("result"));
+        assertEquals("OK", Response.get("result").asText());
+
+        // When I launch robot "R2D2" into the world
+        String Request2 = "{" +
+                "\"robot\": \"R2D2\"," +
+                "\"command\": \"launch\"," +
+                "\"arguments\": [\"shooter\"]" +
+                "}";
+        JsonNode Response2 = serverClient.sendRequest(Request2);
+        System.out.println(Response2);
+        assertNotNull(Response2.get("result"));
+        assertEquals("OK", Response2.get("result").asText());
+
+        // Then the launch should be successful
+        // and a randomly allocated position of R2D2 should be returned.
+        assertNotNull(Response2.get("data"));
+        JsonNode Position = Response2.get("data").get("position");
+        assertNotNull(Position);
+        assertFalse(Position.get(0).asInt() >= 0 && Position.get(0).asInt() < 2);
+        assertFalse(Position.get(1).asInt() >= 0 && Position.get(1).asInt() < 2);
+    }
+
+////        Given a world of size 2x2
+//        assertTrue(serverClient.isConnected());
+//
+////        and robot "HAL" has already been launched into the world
+//
+//            String request = "{" +
+//                    "\"robot\": \"HAL " + x + "\"," +
+//                    "\"command\": \"launch\"," +
+//                    "\"arguments\": [\"shooter\",\"5\",\"5\"]" +
+//                    "}";
+//            serverClient.sendRequest(request);
+//
+//
+////        When I launch robot "R2D2" into the world
+//        String request2 = "{" +
+//                "  \"robot\": \"HAL 10\"," +
+//                "  \"command\": \"launch\"," +
+//                "  \"arguments\": [\"shooter\",\"5\",\"5\"]" +
+//                "}";
+//        JsonNode response = serverClient.sendRequest(request2);
+//        System.out.println(response);
+//        assertNotNull(response.get("result"));
+//        assertEquals("ERROR", response.get("result").asText());
+//
+//    }
+
+//        Then the launch should be successful
+//        and a randomly allocated position of R2D2 should be returned.
+
+
+    @Test
     void worldWithoutObstaclesFull() {
 //        Given a world of size 2x2
         assertTrue(serverClient.isConnected());
@@ -53,6 +120,7 @@ class LaunchRobotTests {
                 "}";
         JsonNode response = serverClient.sendRequest(request2);
 
+
 //        Then I should get an error response back with the message "No more space in this world"
         System.out.println(response);
         assertNotNull(response.get("result"));
@@ -64,7 +132,7 @@ class LaunchRobotTests {
     }
 
     @Test
-    void robotsAvoidObstacle() {
+    void robotsIntoWorldWithObstacle() {
         assertTrue(serverClient.isConnected());
 
         // Launch 8 robots into the world
