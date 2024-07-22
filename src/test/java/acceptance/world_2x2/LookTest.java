d99b48e25cf13b5afeb74362744d0ebd9341b319
tmadondo023 <tmadondo023@student.wethinkcode.co.za>
2024-07-22 09:49:49 +0200

    incomplete LookTest

---
+++ src/test/java/acceptance/world_2x2/LookTest.java
@@ -0,0 +1,58 @@
+package acceptance.world_2x2;
+
+import client.RobotWorldClient;
+import client.RobotWorldJsonClient;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LookTest {
+    private final static int DEFAULT_PORT = 5000;
+    private final static String DEFAULT_IP = "localhost";
+    private final RobotWorldClient serverClient = new RobotWorldJsonClient();
+
+    @BeforeEach
+    void connectToServer(){
+        serverClient.connect(DEFAULT_IP, DEFAULT_PORT);
+    }
+
+    @AfterEach
+    void disconnectFromServer(){
+        serverClient.disconnect();
+    }
+
+    @Test
+    void seeObstacle() {
+//        Given a world of size 2x2
+//        and the world has an obstacle at coordinate [0,1]
+//        and I have successfully launched a robot into the world
+
+        assertTrue(serverClient.isConnected());
+
+        String launchRequest = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"launch\"," +
+                "  \"arguments\": [\"shooter\",\"5\",\"5\"]" +
+                "}";
+        JsonNode launchResponse = serverClient.sendRequest(launchRequest);
+
+        assertNotNull(launchResponse.get("result"));
+        assertEquals("OK", launchResponse.get("result").asText());
+
+//        When I ask the robot to look
+
+        String lookRequest = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"look\"," +
+                "  \"arguments\": []" +
+                "}";
+        JsonNode lookResponse = serverClient.sendRequest(lookRequest);
+
+//        Then I should get a response back with an object of type OBSTACLE at a distance of 1 step.
+
+    }
+}
