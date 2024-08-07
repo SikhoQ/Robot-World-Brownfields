cdb0362eb06280beaf8f00ec51a77103cb017a35
sshabalala023 <sshabalala023@student.wethinkcode.co.za>
2024-08-07 12:30:47 +0200

    removed hardcoded values

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -1,32 +1,68 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DatabaseConnection {
+
+    public static final String DB_NAME = "RobotWorlds.db";
     private static final String URL = "jdbc:sqlite:RobotWorlds.db";
+    public static final String TABLE_CONTACTS = "world";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_SIZE_X = "size_x";
+    public static final String COLUMN_SIZE_Y= "size_y";
+
 
     public static void main(String[] args) {
 
         try (Connection conn = DriverManager.getConnection(URL)) {
             // Load the SQLite JDBC driver (make sure the driver is in your classpath)
             DriverManager.registerDriver(new org.sqlite.JDBC());
-//            Class.forName("org.sqlite.JDBC");
 
             // Establish the connection
-
             Statement statement = conn.createStatement();
-            statement.execute("CREATE TABLE IF NOT EXISTS world (id INTEGER PRIMARY KEY, size_x INTEGER, size_y INTEGER)");
-            statement.execute("INSERT INTO world(id,size_x,size_y) VALUES(16,0,0)");
+            statement.execute("CREATE TABLE IF NOT EXISTS " + TABLE_CONTACTS +
+                    " (" + COLUMN_ID + " integer, " +
+                    COLUMN_SIZE_X + " integer, " +
+                    COLUMN_SIZE_Y+ " integer" +
+                    ")");
+
             statement.execute("SELECT * FROM world");
+            ResultSet results = statement.getResultSet();
+            while(results.next()) {
+                System.out.println(results.getString("id") + " " +
+                        results.getInt("size_x") + " " +
+                        results.getString("size_y"));
+            }
+
+            results.close();
 
             statement.close();
-            conn.close();
 
         } catch (SQLException e) {
             System.out.println("Something went wrong: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
