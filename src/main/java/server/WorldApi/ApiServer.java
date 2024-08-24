c41287b4c87389149c6e0734d802da308280e8ac
sshabalala023 <sshabalala023@student.wethinkcode.co.za>
2024-08-24 14:02:57 +0200

    modified api endpoint for getting world objects of specified world

--- src/main/java/server/WorldApi/ApiServer.java
+++ src/main/java/server/WorldApi/ApiServer.java
@@ -6,6 +6,7 @@ import io.javalin.Javalin;
 
 public class ApiServer {
     private final Javalin apiServer;
+    
     private final ApiHandler apiHandler;
 
     public ApiServer() {
@@ -13,7 +14,7 @@ public class ApiServer {
         this.apiHandler = new ApiHandler();
 
         // Register API endpoints
-        this.apiServer.get("/loadWorld", apiHandler::loadWorld);
+        this.apiServer.get("/world", apiHandler::loadWorld);
         this.apiServer.post("/saveWorld", apiHandler::saveWorld);
         this.apiServer.delete("/removeWorld", apiHandler::removeWorld);
     }
